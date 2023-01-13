1af398e51055d3473e71e1729ff09ae619e13a36
Alex Ricciardi <alex.omegapy@gmail.com>
2023-01-13 11:37:32 -0700

    Added center_window() function

--- my_raylib_functions.hpp
+++ my_raylib_functions.hpp
@@ -34,20 +34,21 @@ https://www.youtube.com/@omegapy
 */
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*--------------------------------------------------------------------
+//--------------------------------------------------------------------- Function pause_screen()
+/*-------------------------------------------------
 
     Pauses the screen, the game
 
- --------------------------------------------------------------------*/
+ --------------------------------------------------*/
 void pause_screen(int pause_key)
 {
-    //----------------------------------------------------
+    //-----------------------------------------------------
 
     // Function operations
     //-----------------------------------------------------
     if (IsKeyPressed(pause_key))
     {
-        //----------------------------------------------------
+        //-----------------------------------------------------
 
         // Variables
         //-----------------------------------------------------
@@ -70,4 +71,21 @@ void pause_screen(int pause_key)
             if (IsKeyPressed(pause_key)) break; // exits while loop and function   
         }
     }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//--------------------------------------------------------------------- Function center_window()
+/*----------------------------------------------------
+
+    Centers window in current monitor
+
+ -----------------------------------------------------*/
+
+void center_window(float window_width, float window_height)
+{
+    int monitor = GetCurrentMonitor();// Get current connected monitor
+    int monitor_width = GetMonitorWidth(monitor); // Get specified monitor width (current video mode used by monitor)
+    int monitor_height = GetMonitorHeight(monitor); // Get specified monitor height (current video mode used by monitor)
+    SetWindowPosition((int)(monitor_width / 2) - (int)(window_width / 2), (int)(monitor_height / 2) - (int)(window_height / 2));
 }
